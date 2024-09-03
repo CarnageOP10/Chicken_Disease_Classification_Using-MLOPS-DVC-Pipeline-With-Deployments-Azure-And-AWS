bf6cc5437c38fbce4a655fbbca171f4a56184cfb
Parth <parthsin10@gmail.com>
2024-09-03 14:45:22 +0530

    Training Phase done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Chicken_Disease_Classification import logger
 from Chicken_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTarinigPipeline
 from Chicken_Disease_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainigPipeline
+from Chicken_Disease_Classification.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -16,6 +17,16 @@ STAGE_NAME = "Prepare Base Model"
 try:
         logger.info(f"Starting {STAGE_NAME}")
         obj = PrepareBaseModelTrainigPipeline()
+        obj.main()
+        logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:
+        logger.error(f"{STAGE_NAME} failed! Error: {str(e)}")
+        raise e
+
+STAGE_NAME = "Training"
+try:
+        logger.info(f"Starting {STAGE_NAME}")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f"Completed {STAGE_NAME}")
 except Exception as e:
